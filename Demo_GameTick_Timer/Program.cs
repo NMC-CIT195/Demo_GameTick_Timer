04b7782fd3b6513bd5a825a1f6bc2751b2deb0ef
John Velis <john@johnvelis.com>
2018-04-02 10:36:39 -0400

    Add project files.

---
+++ Demo_GameTick_Timer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Timers;
+
+namespace Demo_GameTick_Timer
+{
+    class Program
+    {
+        static int gameTick = 1;
+
+        static void Main(string[] args)
+        {
+            Timer gameTickTimer = new Timer(1000);
+
+            gameTickTimer.Elapsed += GameTickTimer_Elapsed;
+
+            gameTickTimer.Start();
+
+            Console.ReadKey();
+        }
+
+        private static void GameTickTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Timer timer = (Timer)sender;
+            TimeSpan gameTime = TimeSpan.FromSeconds(gameTick);
+
+            Console.WriteLine("Real Time: {0:hh:mm:ss:fff}", e.SignalTime);
+            Console.WriteLine("Game Ticks: {0}", gameTick++);
+            Console.WriteLine("Game Time: {0}", gameTime);
+            Console.WriteLine();
+
+            if (gameTick > 5)
+            {
+                gameTick = 1;
+                timer.Start();
+            }
+        }
+    }
+}
